adc8163a37f1319d53ad238d90bf50844e68be3c
Leticia Betman <leticiarodriguesbetman@gmail.com>
2025-08-05 10:00:06 -0300

    reorganiza arquivos

--- src/tema/controllers/tema.controller.ts
+++ src/tema/controllers/tema.controller.ts
@@ -10,7 +10,7 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { Tema } from '../../postagem/entities/tema.entity';
+import { Tema } from '../entities/tema.entity';
 import { TemaService } from '../services/tema.service';
 
 @Controller('/temas')
